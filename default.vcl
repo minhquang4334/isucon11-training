a7a427f81fdab1a6a16c353b31133d7c4c32917f
minhquang4334 <minhquang4334@gmail.com>
2021-10-17 10:36:38 +0900

    parameter調整する

--- default.vcl
+++ default.vcl
@@ -33,7 +33,7 @@ sub vcl_backend_response {
     #
     # Here you clean the response headers, removing silly Set-Cookie headers
     # and other mistakes your backend does.
-    set beresp.ttl = 0.8s;
+    set beresp.ttl = 0.5s;
     set beresp.grace = 0.2s;
 }
 
--- ruby/app.rb
+++ ruby/app.rb
@@ -168,7 +168,7 @@ module Isucondition
         warn 'start scheduler'
         scheduler = Rufus::Scheduler.new
 
-        scheduler.every '1s' do
+        scheduler.every '0.3s' do
           warn 'run scheduler'
           # Get data from cache
           # insert
